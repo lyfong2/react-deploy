cb1ffe22dd83c4cfbd0e120737b14e840b9a4b54
Danny Lin <dannyjabez@gmail.com>
2020-10-26 17:32:30 -0400

    deploy my react ap to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import profileImage from './img/girl.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Danny React App</h1>
+      <h2>Author: Danny Lin</h2>
+      <img src={profileImage} alt="profile-image" />
     </div>
   );
 }
